d74ddbac8379a9f4d0d9e2449374bf684e9cd760
farees hussain <fareezzhussain@gmail.com>
2020-09-16 01:16:32 +0530

    emit messages 2

--- public/chat.js
+++ public/chat.js
@@ -3,14 +3,15 @@ var socket = io.connect("http://localhost:3030")
 
 //* Query DOM
 
-var message = document.getElementById('message')
-var handle = document.getElementById('handle')
-var btn = document.getElementById('send')
-var output = document.getElementById('output')
+var message = document.getElementById('message');
+ handle = document.getElementById('handle'),
+ btn = document.getElementById('send'),
+ output = document.getElementById('output');
 
 //* Emit events
 
 btn.addEventListener('click',()=>{
+    console.log('button clicked')
     socket.emit('chat',{
         message:message.value,
         handle:handle.value
@@ -19,5 +20,6 @@ btn.addEventListener('click',()=>{
 
 //* listen for events
 socket.on('chat',(data)=>{
+    console.log(data)
     output.innerHTML+='<p><strong>'+data.handle+':</strong>'+data.message+'</p>'
 })
